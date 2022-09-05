8d6adffdc3f14359d3a2fda687f7ec9e29274c7e
Ifeanyi <chimaifeanyi29@gmail.com>
2022-09-05 22:18:51 +0100

    add glitch url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 // import { AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 // const BASE_URL = "http://localhost:3500"
-const BASE_URL = "https://ifeanyi-student-backend.herokuapp.com/"
+// const BASE_URL = "https://ifeanyi-student-backend.herokuapp.com/"
+const BASE_URL = "https://ifeanyi-student-backend.glitch.me/"
 
 export default axios.create({
     baseURL: BASE_URL
